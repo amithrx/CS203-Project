0f5db3841c81a0379aa65190875b5b71ea95e316
Amit kumar <92815147+amithrx@users.noreply.github.com>
2021-11-21 21:11:37 +0530

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -517,3 +517,31 @@ module.exports = class QuineMcCluskey {
     return result;
   }
 };
+
+
+
+const Minterm = require("./minterm.js");
+
+function decToBin(value) {
+  return (value >>> 0).toString(2);
+}
+
+function valueIn(value, array) {
+  for (const compare of array) {
+    if (compare == value) {
+      return true;
+    }
+
+    if (value instanceof Minterm) {
+      if (compare.equals(value)) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+module.exports = {
+  decToBin,
+  valueIn,
+};
